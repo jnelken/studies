439f46b0b746de01dfe4a40658bc3ac25ed76c2f
Jake Nelken <jnelks@gmail.com>
2015-12-15 22:34:03 -0500

    Remove duplicates.

--- bitly-clone/bitly-clone/config/initializers/session_store.rb
+++
@@ -1,3 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-UrlShortener::Application.config.session_store :cookie_store, key: '_UrlShortener_session'
--- bitly-clone/bitly-clone/db/migrate/20151014181018_add_index_to_short_url_on_shortened_urls.rb
+++
@@ -1,7 +0,0 @@
-class AddIndexToShortUrlOnShortenedUrls < ActiveRecord::Migration
-  def change
-    add_index :shortened_urls, :short_url, unique: true
-    remove_index :shortened_urls, :submitter_id
-    add_index :shortened_urls, :submitter_id
-  end
-end
